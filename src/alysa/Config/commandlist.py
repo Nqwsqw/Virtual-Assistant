f833669235e36ce88d2ad70a9547f6b300ce9e30
Nqwsqw <vdugoua@gmail.com>
2018-08-19 00:32:59 +0200

    update

--- src/alysa/Config/commandlist.py
+++ src/alysa/Config/commandlist.py
@@ -5,4 +5,6 @@ from alysa.Command import defaultcommand
 list_command = [
 	("src/alysa/Command/tags.json", defaultcommand.printHello, "printHello"),
 	("src/alysa/Command/tags.json", defaultcommand.saySomething, "saySomething"),
+	("src/alysa/Command/tags.json",
+defaultcommand.playMusic, "playMusic"),
 ]
