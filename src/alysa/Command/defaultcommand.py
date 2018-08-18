4eb18550a7937ad6750f9855a0d10314e3fc5e11
Vincent5883 <vdugoua@gmail.com>
2018-08-18 18:53:03 +0200

    update

--- src/alysa/Command/defaultcommand.py
+++ src/alysa/Command/defaultcommand.py
@@ -1,3 +1,6 @@
+from pathlib import path
+import os
+
 def printHello(request):
 	print("Hello World")
 
@@ -6,3 +9,14 @@ def saySomething(request):
 	string = request.split("say ")
 	string = string[1]
 	print(string)
+
+def playMusic(request):
+	string = request["auto"]
+	string = string.replace("play ", "")
+	extension = "mp3"
+	path = "Virutal-Assistant/res/Musics/" + string + "." + extension
+	music = open(path, "r")
+	if(os.path.isfile(path) == True):
+		os.startfile(path)
+	else:
+		print("No Such Music")
